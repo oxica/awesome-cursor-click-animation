be2816fcede1e204dda13c2931b9fdbc13e5f5e8
Oksana Banshchykova <88445174+oxica@users.noreply.github.com>
2023-01-25 00:25:50 +0100

    02

---
+++ script.js
@@ -0,0 +1,28 @@
+const cursorAnimation = document.querySelector(".cursor");
+const cursors = document.querySelectorAll(".cursor")
+
+// document.addEventListener('mousemove', (e) => {
+document.addEventListener('click', (e) => {
+    let x = e.clientX;
+    let y = e.clientY;
+
+    cursorAnimation.style.top = y + "px";
+    cursorAnimation.style.left = x + "px";
+
+    cursors.forEach((cursor) => {
+        cursor.classList.add("active");
+
+        function removeCursorActive() {
+            cursor.classList.remove("active");
+        }
+
+        setTimeout(removeCursorActive, 1000)
+    });
+
+    let cursorClone = cursorAnimation.cloneNode(true);
+    document.querySelector("body").appendChild(cursorClone);
+
+    setTimeout(() => {
+        cursorClone.remove();
+    }, 1000);
+})
